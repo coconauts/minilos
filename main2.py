d78d1092cfbca582c1cadf91f1a12242c635b7b6
Javier <rephus@gmail.com>
2021-02-16 14:41:56 +0100

    Add new albums

--- main2.py
+++ main2.py
@@ -9,10 +9,18 @@ NFC_TO_ALBUM = {
     390: "spotify:album:1kCHru7uhxBUdzkm4gzRQc", # hamilton 
     927: "spotify:playlist:1kq332GEYrX6D5vrszgDUF", # dropmix
     460: "spotify:playlist:0uEOz6n3BoNGa39CQVtsbH", # life is strange
-    997: "spotify:playlist:37i9dQZF1DZ06evO2hOiea", # nobuo uematsu
     530: "spotify:playlist:37i9dQZF1DWSNC7AjZWNry", # queen
+    
+    509: "spotify:album:1KCiWQIQtagNrWcJvPTiNe", #trance best of 
+    423: "spotify:playlist:3NkBXHqVwjeyu5fcBcmIRE", #bioshock inspired
+    983: "spotify:playlist:684nXqMbtn1gr1sNSk1MsI", #rock and roll
+    176: "spotify:playlist:5gIPHDwAG2QN38CIdRW2y8", #persona 5
+    131: "spotify:album:4CuBPBeJ8ncHwgQhIgvx2j", #destiny 2 : forsaken
+    835: "spotify:album:1BNQflQCNLW3h9EMWZqpHm", # gentle love: game music lullabies
+
 }
- 
+
+last_uri = ""
 
 try:
     while True:
@@ -24,11 +32,12 @@ try:
         spotify_uri = NFC_TO_ALBUM.get(last_3_chars)
         if not spotify_uri: 
             print("NFC not in dict {}".format(last_3_chars))
-        else: 
+        elif spotify_uri != last_uri: # Don't play it twice
+            last_uri = spotify_uri
             print("Playing {}".format(spotify_uri))
             spotify.play(spotify_uri)
 
-        sleep(1)
+        sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
     raise
